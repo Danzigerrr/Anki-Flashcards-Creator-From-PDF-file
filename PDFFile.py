24c826e6e6ccb63506132868dcc72d10d49fb96a
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-11-11 22:17:39 +0100

    add extracting text from each page of pdf file

--- PDFFile.py
+++ PDFFile.py
@@ -1,9 +1,14 @@
 class PDFFile:
-    images = []
+    content = []
 
     def __init__(self, saving_dir, filename):
         self.saving_dir = saving_dir
         self.filename = filename
 
-    def myfunc(self):
-        print("Hello my name is " + self.name)
+    def get_file_location(self):
+        return self.saving_dir + '/' + self.filename
+
+    def get_filename_without_extension(self):
+        return self.filename.split('.')[0] + '/'
+
+
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from PDFFile import PDFFile
 from pdf2image import convert_from_path
 import os
+import PyPDF2
 
 
-def convert_pdf_file_to_images(pdf_file):
+def extract_images_from_pdf_file(pdf_file):
     images = convert_from_path(pdf_file.saving_dir + '/' + pdf_file.filename)
 
-    saving_directory = 'images/' + pdf_file.filename.split('.')[0] + '/'
+    saving_directory = 'images/' + pdf_file.get_filename_without_extension()
 
     # create directory if it does not exist
     if not os.path.exists(saving_directory):
@@ -16,7 +17,21 @@ def convert_pdf_file_to_images(pdf_file):
     for i in range(len(images)):
         image_saving_dir = saving_directory + 'page' + str(i) + '.jpg'
         images[i].save(image_saving_dir, 'JPEG')
-        pdf_file.images.append(image_saving_dir)
+        pdf_file.content.append({'image': image_saving_dir})
+
+    print('Successfully converted each pdf page into an image')
+
+
+def extract_text_from_pdf_file(pdf_file):
+    with open(pdf_file.get_file_location(), 'rb') as file_to_convert:
+        pdf_reader = PyPDF2.PdfReader(file_to_convert)
+
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            text = page.extract_text()
+            pdf_file.content[page_num].update({'text': text})
+
+        print('Successfully extracted text from pdf file')
 
 
 if __name__ == '__main__':
@@ -25,6 +40,6 @@ if __name__ == '__main__':
 
     pdf_file = PDFFile(dir_with_pdf, filename)
 
-    convert_pdf_file_to_images(pdf_file)
+    extract_images_from_pdf_file(pdf_file)
 
-    print(f'Saved {len(pdf_file.images)} images in directory ')
+    extract_text_from_pdf_file(pdf_file)
